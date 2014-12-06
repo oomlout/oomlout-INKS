cbd357db0a66ebd9862f387f4a47f7cd9ba0e714
oomlout <aaron@oomlout.com>
2014-12-06 16:20:49 +0000

    Added option to filter files.

--- INKSmain.py
+++ INKSmain.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(description='OOMLOUT-IMAG -- Image Resolution Tool')
 parser.add_argument('-id','--iLocation', help='InkScape location', required=False)
 parser.add_argument('-bd','--directory', help='directory to recursivly go through to generate PDFs for', required=False)
-
+parser.add_argument('-fi','--filter', help='filter make sure filename includes this text', required=False)
 args = vars(parser.parse_args())
 
 #loading variables from comman line
@@ -23,7 +23,7 @@ args = vars(parser.parse_args())
 
 
 #
-def INKSgeneratePDFs(iLocation, directoryName):
+def INKSgeneratePDFs(iLocation, directoryName, filter):
 	"Generating Resolutions for: " + directoryName
 	for root, _, files in os.walk(directoryName):
 		for f in files:
@@ -37,7 +37,8 @@ def INKSgeneratePDFs(iLocation, directoryName):
 			#time.sleep(1)
 
 			#make +01 etc okay (fails if more than 10 images
-			if type.lower() in ".svg":
+			print f
+			if type.lower() in ".svg" and filter in f:
 				print "    Generating pdf File: " + f 
 				execLine = iLocation + " -f " + '"' + fullName + '" -A "' + fullName.split(".")[0] + '.pdf"'
 				print "Executing " + execLine
@@ -54,8 +55,13 @@ if args['directory'] <> None:
 	directoryName = args['directory']
 	print "Genrating PDFs for Directory: " + directoryName
 
+filter = ""
+if args['filter'] <> None:
+	filter = args['filter']
+	print "Filter: " + filter
+
 if iLocation <> "" and directoryName <> "":
-	INKSgeneratePDFs(iLocation, directoryName)
+	INKSgeneratePDFs(iLocation, directoryName, filter)
 
 
 
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# oomlout-TPGE
+# oomlout-INKS
 A command line python tool used for generating pdfs from svgs using inkscape.
 
 ## Command Line Parameters
 
 * -id				-- inkscape location
 * -di				-- directory to iterate through
+* -fi				-- filter make sure filename includes this text
 
 
